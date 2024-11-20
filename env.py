b314142772a0e46c715dbb817f17d15f51ee1dc8
damian-bots <tosu0791@gmail.com>
2024-11-21 01:30:40 +0530

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip() # Not a necessary variable anymore but you can add to get stats
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+API_ID = os.getenv("API_ID", "24620300").strip()
+API_HASH = os.getenv("API_HASH", "9a098f01aa56c836f2e34aee4b7ef963").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6865478876:AAETvvxfc-lWp5rHl4l0JddLyjhu8JRyu2s").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "postgres://wsnioboi:Fkf1ef7qRJVP1CvP_gKYqhdxk6LXBbQE@balarama.db.elephantsql.com/wsnioboi").strip() # Not a necessary variable anymore but you can add to get stats
+MUST_JOIN = os.getenv("MUST_JOIN", "the_hogwart")
 
 if not API_ID:
     raise SystemExit("No API_ID found. Exiting...")
